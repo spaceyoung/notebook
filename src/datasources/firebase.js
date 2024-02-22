222280c65f57dc2c471b3797143475d300b6e84a
minyoung <myoung.ever@gmail.com>
2024-02-22 14:19:10 +0900

    🐛 Fix: import getAuth by 'firebase/auth'
    - firebase에서 getAuth 불러오기 추가

--- src/datasources/firebase.js
+++ src/datasources/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore, collection, query, orderBy } from 'firebase/firestore';
 
 const notebookDB = initializeApp({
