c471f3d7208510c94af06f872ad9782e3c581954
minyoung <myoung.ever@gmail.com>
2024-03-21 05:07:52 +0900

    🐛 Fix: change v-if conditions when no user record data
    - 독서 기록 데이터가 없을 경우 나타낼 v-if 조건 변경

--- src/components/tab/MyReadingEnd.vue
+++ src/components/tab/MyReadingEnd.vue
@@ -1,7 +1,7 @@
 <template>
   <Loading v-if="isLoading" />
   <div v-else class="view d-flex flex-1-0 flex-column">
-    <div v-if="myReadingEndList.length === 0" class="d-flex justify-center align-center">
+    <div v-if="!myReadingEndList" class="d-flex justify-center align-center">
       <p><span>읽은 책을 추가해서</span>나만의 독서 기록을 만들어봐요😉</p>
     </div>
     <v-list v-else class="d-flex flex-wrap pa-0 pt-8 px-sm-8">
@@ -19,6 +19,7 @@ import MyReadingEndBook from '@/components/card/MyReadingEndBook.vue';
 const { inquiryMyReadingEnd } = useRecordStore();
 const isLoading = computed(() => useRecordStore().isLoading);
 const myReadingEndList = computed(() => useRecordStore().myReadingEndList);
+
 inquiryMyReadingEnd();
 </script>
 
