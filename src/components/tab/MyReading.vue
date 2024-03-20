4b651f98da9875fee52832408e99de510f41caf9
minyoung <154400292+spaceyoung@users.noreply.github.com>
2024-03-21 05:35:58 +0900

    🐛 Fix: remove user record data loading delay (#27)
    - 독서 기록 데이터 조회 시 설정한 로딩 지연 시간 제거

--- src/components/tab/MyReading.vue
+++ src/components/tab/MyReading.vue
@@ -1,7 +1,7 @@
 <template>
   <Loading v-if="isLoading" />
   <div v-else class="view d-flex flex-1-0 flex-column">
-    <div v-if="myReadingList.length === 0" class="d-flex justify-center align-center">
+    <div v-if="!myReadingList" class="d-flex justify-center align-center">
       <p><span>읽은 책을 추가해서</span>나만의 독서 기록을 만들어봐요😉</p>
     </div>
     <v-list v-else class="d-flex flex-wrap pa-0 pt-8 px-sm-8">
@@ -19,6 +19,7 @@ import MyReadingBook from '@/components/card/MyReadingBook.vue';
 const { inquiryMyReading } = useRecordStore();
 const isLoading = computed(() => useRecordStore().isLoading);
 const myReadingList = computed(() => useRecordStore().myReadingList);
+
 inquiryMyReading();
 </script>
 
