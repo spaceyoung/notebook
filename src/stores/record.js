ed940f80d909eed53dc0bc9dbfcf06c9cd4f2013
minyoung <myoung.ever@gmail.com>
2024-03-23 16:42:25 +0900

    ♻️ Refactor: declare record book info class
    - 기록할 도서 정보 인스턴스 객체를 생성할 클래스 선언

--- src/stores/record.js
+++ src/stores/record.js
@@ -6,25 +6,31 @@ import { collection, doc, setDoc, deleteDoc, updateDoc } from 'firebase/firestor
 
 // 로그인한 사용자 정보 불러오기
 import { useMemberStore } from '@/stores/member';
-const currentUser = computed(() => useMemberStore().currentUser);
+const memberStore = useMemberStore();
+const currentUser = computed(() => memberStore.currentUser);
+
+// 기록할 도서 정보 클래스 선언
+class recordBookInfo {
+  constructor(platform, readingState, readingStartDate, formattedReadingStartDate, readingEndDate, formattedReadingEndDate, readingPage, rating, sentence, review) {
+    this.platform = platform;
+    this.readingState = readingState;
+    this.readingStartDate = readingStartDate;
+    this.formattedReadingStartDate = formattedReadingStartDate;
+    this.readingEndDate = readingEndDate;
+    this.formattedReadingEndDate = formattedReadingEndDate;
+    this.readingPage = readingPage;
+    this.rating = rating;
+    this.sentence = sentence;
+    this.review = review;
+  }
+};
 
 export const useRecordStore = defineStore('record', () => {
   const state = reactive({
     isLoading: false,
     myReadingList: [],
     myReadingEndList: [],
-    recordBookDefault: {
-      platform: null,
-      readingState: null,
-      readingStartDate: null,
-      readingEndDate: null,
-      formattedReadingStartDate: null,
-      formattedReadingEndDate: null,
-      readingPage: null,
-      rating: 0,
-      sentence: '',
-      review: '',
-    },
+    recordBookDefaultInfo: new recordBookInfo(null, null, null, null, null, null, null, null, null, null),
   });
   const isLoading = computed(() => state.isLoading);
   const myReadingList = computed(() => state.myReadingList);
@@ -68,18 +74,18 @@ export const useRecordStore = defineStore('record', () => {
   // 데이터베이스 myReading 독서 중 기록 수정
   const updateMyReading = async (id, myReadingItem) => {
     const docRef = doc(database, 'users', currentUser.value.uid, 'myReading', id);
-    await updateDoc(docRef, {
-      platform: myReadingItem.platform,
-      readingState: myReadingItem.readingState,
-      readingStartDate: myReadingItem.readingStartDate,
-      formattedReadingStartDate: myReadingItem.formattedReadingStartDate,
-      readingEndDate: myReadingItem.readingEndDate,
-      formattedReadingEndDate: myReadingItem.formattedReadingEndDate,
-      readingPage: myReadingItem.readingPage,
-      rating: myReadingItem.rating,
-      sentence: myReadingItem.sentence,
-      review: myReadingItem.review,
-    });
+    await updateDoc(docRef, new recordBookInfo(
+      myReadingItem.platform,
+      myReadingItem.readingState,
+      myReadingItem.readingStartDate,
+      myReadingItem.formattedReadingStartDate,
+      myReadingItem.readingEndDate,
+      myReadingItem.formattedReadingEndDate,
+      myReadingItem.readingPage,
+      myReadingItem.rating,
+      myReadingItem.sentence,
+      myReadingItem.review,
+    ));
   };
 
   // 데이터베이스 myReading에서 독서 중 기록 삭제
@@ -98,18 +104,18 @@ export const useRecordStore = defineStore('record', () => {
   // 데이터베이스 myReadingEnd 독서 완료 기록 수정
   const updateMyReadingEnd = async (id, myReadingEndItem) => {
     const docRef = doc(database, 'users', currentUser.value.uid, 'myReadingEnd', id);
-    await updateDoc(docRef, {
-      platform: myReadingEndItem.platform,
-      readingState: myReadingEndItem.readingState,
-      readingStartDate: myReadingEndItem.readingStartDate,
-      formattedReadingStartDate: myReadingEndItem.formattedReadingStartDate,
-      readingEndDate: myReadingEndItem.readingEndDate,
-      formattedReadingEndDate: myReadingEndItem.formattedReadingEndDate,
-      readingPage: myReadingEndItem.readingPage,
-      rating: myReadingEndItem.rating,
-      sentence: myReadingEndItem.sentence,
-      review: myReadingEndItem.review,
-    });
+    await updateDoc(docRef, new recordBookInfo(
+      myReadingEndItem.platform,
+      myReadingEndItem.readingState,
+      myReadingEndItem.readingStartDate,
+      myReadingEndItem.formattedReadingStartDate,
+      myReadingEndItem.readingEndDate,
+      myReadingEndItem.formattedReadingEndDate,
+      myReadingEndItem.readingPage,
+      myReadingEndItem.rating,
+      myReadingEndItem.sentence,
+      myReadingEndItem.review,
+    ));
   };
 
   // 데이터베이스 myReading에서 독서 완료 기록 삭제
